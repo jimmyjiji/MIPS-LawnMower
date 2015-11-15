f1dd6e2f3fe183ca11453cb9af9cca8817a8f909
Jimmy <jimmy.ji@stonybrook.edu>
2015-11-14 22:00:30 -0500

    search method for matching 2 bits in file is done

--- hw3.asm
+++ hw3.asm
@@ -14,8 +14,8 @@ arrayFill:
 	sw $s0, 0($sp)
 	sw $s1, 4($sp)
 	sw $s2, 8($sp)
-	sw $s3, 12($sp)
-				#push used s variables onto the stack 
+	sw $s3, 12($sp)		#push used s variables onto the stack 
+				
 				
 	la $s0, ($a0)		#set starting address of array to s0
 	la $s1, ($a1)		#set s1 to address of file name 
@@ -41,8 +41,6 @@ arrayFill:
 	li $t0, 0		#set counter for maxBytes check
 	li $t1, 0		#set counter for return 
 	
-	li $v0, 4
-	syscall 
 	la $t2, buffer
 	write:	
 		
@@ -62,6 +60,12 @@ close_and_end:
 	move $a0, $s3
 	syscall 
 	
+	#################### test if read
+	#li $v0, 4
+	#la $a0, buffer
+	#syscall 
+	###################
+	
 	lw $s0, 0($sp)
 	lw $s1, 4($sp)
 	lw $s2, 8($sp)
@@ -80,9 +84,100 @@ error:
 	jr $ra 
 
 find2Byte:
-	#Define your code here
+	sw $s0, 0($sp)
+	sw $s1, 4($sp)
+	sw $s2, 8($sp)
+	sw $s3, 12($sp)		
+	sw $s4, 16($sp)
+	sw $s5, 20($sp)		#push used s variables onto the stack 
 	
-
+	#Define your code here
+	la $s0, ($a0)		#set starting address of array to s0
+	li $t7, 2		#set t7 to 2
+	move $s1, $a1 
+	addi $s2, $a2, 0	#set s2 to amount of rows in a2
+	mul  $s3, $a3, $t7	#set s3 to amount of columns in a3
+	
+	andi $t0, $s1, 65280 	# first byte from s1 to t0
+	sra $t0, $t0, 8
+	andi $t1, $s1, 255	# second byte from s1 to t1
+	
+	move $t2, $t1		#swap bytes because of endianess 
+	move $t1, $t0
+	move $t0, $t2
+	
+	######################################## test
+	
+	li $v0, 4
+	la $a0, nextline
+	syscall
+	
+	li $v0, 11
+	move $a0, $t0
+	syscall
+	move $a0, $t1
+	syscall
+	
+	########################################## test
+	li $t2, 0 		# row counter 
+	li $t3, 0		# column counter 
+	row:
+		
+		bge $t2, $s2, endLoop		#if i = row amount, end loop 
+		
+		column: 
+			bge $t3, $s3, nextRow
+			lb $t4, ($s0)		#t4 is set to bit of address
+			bne $t0, $t4, add2	#if the first bit isn't equal, no need to check y, jump 2 bits
+			beq $t0, $t4, eqfirstbit
+		
+		add2:
+			addi $t3, $t3, 2
+			addi $s0, $s0, 2
+			j column
+		add1:
+			addi $t3, $t3, 1
+			addi $s0, $s0, 1
+			j column
+		eqfirstbit:
+			move $s4, $t2		#set s4 to row#
+			addi $t3, $t3, 1
+			addi $s0, $s0, 1
+			lb $t4, ($s0)
+			beq $t1, $t4, eqsecondbit
+			j add1
+		eqsecondbit:
+			move $s5, $t3		#set s5 to column#
+			move $v0, $s4	
+			addi $v1, $s5, -1
+			div $v1, $t7		#divide columns by 2
+			mflo $v1		#set v1 to quotient in mflo. 
+			
+			lw $s0, 0($sp)
+			lw $s1, 4($sp)
+			lw $s2, 8($sp)
+			lw $s3, 12($sp)		
+			lw $s4, 16($sp)
+			lw $s5, 20($sp)		#pull used s variables onto the stack 
+			
+			jr $ra
+	nextRow:
+		addi $t2, $t2 1
+		li $t3, 0
+		j row
+			
+	endLoop:
+	li $v0, -1
+	li $v1, -1
+	
+	lw $s0, 0($sp)
+	lw $s1, 4($sp)
+	lw $s2, 8($sp)
+	lw $s3, 12($sp)		
+	lw $s4, 16($sp)
+	lw $s5, 20($sp)		#pop used s variables onto the stack 
+	
+	jr $ra
 ##############################
 # PART 2/3 FUNCTION
 ##############################
@@ -95,4 +190,5 @@ playGame:
 .data
 	#Define your memory here 
 
-buffer: .space 10000
+buffer: .space 4000
+nextline: .asciiz "\n"
--- main.asm
+++ main.asm
@@ -6,15 +6,31 @@ main:
  
 	la $a0, 4294901760
 	la $a1, filename
-	li $a2, 100000
+	li $a2, 4000
 	jal arrayFill
+	
+	
+	la $a0, 4294901760 #base address  
+	lh $a1, shortvalue
+	li $a2, 25	#rows
+	li $a3, 80	#columns 
+	jal find2Byte
+	
+	
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	
+	move $a0, $v1
+	syscall
+	
 	
 	li $v0, 10
 	syscall 
 
 
 .data 
-filename: .asciiz "hw3/partial_filled_green.map" 
-
+filename: .asciiz"//files/UndergradHome/jjji/Desktop/hw3/landscape1.map" 
+shortvalue: .asciiz "asxx"	#searching for " z"
 
 .include "hw3.asm"
